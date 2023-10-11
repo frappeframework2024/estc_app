9d2a2e184e2a12728963621ab985ab99436a7647
Pheakdey <frapperpnext@gmail.com>
2023-10-11 13:52:32 +0700

    update

--- estc_app/estc_hr/doctype/leave_request/leave_request_list.js
+++ estc_app/estc_hr/doctype/leave_request/leave_request_list.js
@@ -1,17 +1,11 @@
 frappe.listview_settings['Leave Request'] = {
-    get_indicator: function(doc) {
-		const status_colors = {
-			"Draft": "yellow",
-			"Submitted": "blue",
-			"Write Off": "red",
-		};
-		return [__(doc.status), status_colors[doc.status], "status,=,"+doc.status];
-	},
     formatters: {
         photo: function (value, field, doc) {
+            return `<img src='${value || "/assets/estc_app/images/avatar-2.png"}' style='border-radius: 50%;height:35px; margin-right:10px;margin-left:5px'/>`;
+        },
+		status: function (value, field, doc) {
             return `<img src='${doc.photo || "/assets/estc_app/images/avatar-2.png"}' style='border-radius: 50%;height:35px; margin-right:10px;margin-left:5px'/>`;
         },
-        
-
+		
     },
 }
