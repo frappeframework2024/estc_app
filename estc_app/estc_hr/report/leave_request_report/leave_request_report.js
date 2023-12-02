0a9eec4c54774223e72742e9a09536991e91ab95
frappeerpnext <frapperpnext@gmail.com>
2023-12-02 17:01:28 +0700

    update

--- estc_app/estc_hr/report/leave_request_report/leave_request_report.js
+++ estc_app/estc_hr/report/leave_request_report/leave_request_report.js
@@ -1,8 +1,26 @@
-// Copyright (c) 2023, Tes Pheakdey and contributors
-// For license information, please see license.txt
-
 frappe.query_reports["Leave Request Report"] = {
+	
 	"filters": [
+		{
+			fieldname: "fiscal_year",
+			label: "Academic Year",
+			fieldtype: "Link",
+			options: "Fiscal Year",
+			mandatory:1
+		},
+		{
+			fieldname: "employee",
+			label: "Employee",
+			fieldtype: "Link",
+			options: "Employee",
+			mandatory:1
+		},
+	],
+	"formatter": function(value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
 
-	]
+		
+		
+		return value;
+	},
 };
--- estc_app/estc_hr/report/leave_request_report/leave_request_report.py
+++ estc_app/estc_hr/report/leave_request_report/leave_request_report.py
@@ -1,9 +1,50 @@
-# Copyright (c) 2023, Tes Pheakdey and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
+from datetime import datetime,timedelta
 
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+	
+	return get_columns(filters), get_data(filters)
+
+def get_columns(filters):
+	columns=[]
+	columns.append({'fieldname':"name",'label':"Request No",'fieldtype':'Link','options':'Leave Request','align':'center','width':130})
+	columns.append({'fieldname':"employee",'label':"Employee",'fieldtype':'Link','options':'Employee','align':'left','width':130})
+	columns.append({'fieldname':"leave_type",'label':"Leave Type",'fieldtype':'Data','align':'left','width':130})
+	columns.append({'fieldname':"posting_date",'label':" Posting Date",'fieldtype':'Date','align':'left','width':130})
+	columns.append({'fieldname':"start_date",'label':"Start",'fieldtype':'Date','align':'center','width':130})
+	columns.append({'fieldname':"to_date",'label':"To",'fieldtype':'Date','align':'center','width':130})
+	columns.append({'fieldname':"total_leave_days",'label':"Total Leave",'fieldtype':'Data','css_class':'text-bold','align':'center','width':130})
+	
+	return columns
+
+def get_data(filters):
+	data = []
+	sql = """
+			select 
+				name,
+				employee,
+				leave_type,
+				posting_date,
+				start_date,
+				to_date,
+				total_leave_days
+			from 
+				`tabLeave Request` a
+			{0}
+	""".format(get_conditions(filters))
+	data = frappe.db.sql(sql,filters,as_dict=1)
+	return data
+
+
+def get_conditions(filters):
+	select_filters = []
+	fiscal_year=frappe.get_last_doc('Fiscal Year')
+	select_filters.append("fiscal_year = %(fiscal_year)s")
+	select_filters.append("status = 'Approved'")
+	select_filters.append("employee = %(employee)s")
+		
+	if len(select_filters) > 0:
+		return " WHERE " + " AND ".join(select_filters)
+	else:
+		return ''
