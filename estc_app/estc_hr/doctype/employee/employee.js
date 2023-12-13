d8947b518400fbcd7665ff78eb84d23bb01b6492
frappeerpnext <frapperpnext@gmail.com>
2023-12-13 10:47:42 +0700

    update

--- estc_app/estc_hr/doctype/employee/employee.js
+++ estc_app/estc_hr/doctype/employee/employee.js
@@ -3,17 +3,23 @@
 
 frappe.ui.form.on("Employee", {
 	refresh(frm) {
-        frappe.call("estc_app.estc_hr.doctype.employee.employee.get_current_employee_leave_balance").then(r=>{
-            console.log(r.message)
-            frm.dashboard.add_indicator(__("AL: "+r.message.max_leave) ,"red")
-            frm.dashboard.add_indicator(__("Used: "+r.message.use_leave) ,"green") 
-            frm.dashboard.add_indicator(__("Balance: "+r.message.balance) ,"blue") 
+        frappe.call({
+            "method": 'estc_app.estc_hr.doctype.employee.employee.get_current_employee_leave_balance',
+            args: {
+                "name": frm.doc.name,
+               
+            },
+            callback: function (r) {
+                frm.dashboard.add_indicator(__("AL: "+r.message.max_leave) ,"red")
+                frm.dashboard.add_indicator(__("Used: "+r.message.use_leave) ,"green") 
+                frm.dashboard.add_indicator(__("Balance: "+r.message.balance) ,"blue") 
 
-            frm.dashboard.add_indicator(__("Sick Leave: "+r.message.max_sick_leave) ,"red")
-            frm.dashboard.add_indicator(__("Sick Leave Used: "+r.message.use_sick_leave) ,"green") 
-            frm.dashboard.add_indicator(__("Sick Leave Balance: "+r.message.sick_leave_balance) ,"blue") 
-
-    });
+                frm.dashboard.add_indicator(__("Sick Leave: "+r.message.max_sick_leave) ,"red")
+                frm.dashboard.add_indicator(__("Sick Leave Used: "+r.message.use_sick_leave) ,"green") 
+                frm.dashboard.add_indicator(__("Sick Leave Balance: "+r.message.sick_leave_balance) ,"blue") 
+            }
+        });
+        
     frm.set_query("department", function () {
         return {
             "filters": {
