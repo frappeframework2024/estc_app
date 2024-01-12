0de2fd8008ffc709ca746619cae363cb65af99d2
outdomleap <outdom.leap@esoftix.com>
2024-01-12 10:58:57 +0700

    update

--- estc_app/estc_hr/doctype/attendance/attendance.py
+++ estc_app/estc_hr/doctype/attendance/attendance.py
@@ -15,41 +15,38 @@ def insert_absent_attendance_queue():
 def insert_attendance():
 	sql = """
 		select 
-			name,
-			attendance_device_id,
-			department,
-			photo
-		from `tabEmployee`
-		where has_no_attendance_device_id = 0 and
-			name not in (select 
-							employee 
-						from `tabAttendance` 
-						where DATE(attendance_date) = '{}')
+			a.name,
+			a.attendance_device_id,
+			a.department,
+			a.photo,
+			b.shift
+		from `tabEmployee` a
+		right join `tabShift Assignment` b on a.name = b.employee
+		where a.has_no_attendance_device_id = 0 and
+	a.name not in (select employee from `tabAttendance` att where DATE(att.attendance_date) = '{}' and att.shift = b.shift)
 	""".format(datetime.now().date())
 	employee_list_not_check_in = frappe.db.sql(sql,as_dict=1)
 	
 	default_fiscal_year = frappe.db.get_value("Fiscal Year",{"is_default":1})
 	for emp in employee_list_not_check_in:
-		shift_assignment = frappe.db.get_value('Shift Assignment', {'employee': emp.name,'fiscal_year': default_fiscal_year}, ['holiday','shift'],as_dict=1)
-		if shift_assignment:
-			day_off = frappe.db.sql("""select name from `tabHoliday Schedule` where parent = '{0}' and date = '{1}' and is_day_off = 1""".format(shift_assignment.holiday,datetime.now().date()))
-			working_shift=frappe.db.get_value("Working Shift",shift_assignment.shift,['name','late_time','on_duty_time','off_duty_time','beginning_in','ending_in','beginning_out','ending_out','leave_early_time'],as_dict=1)
-			if len(day_off)==0:
-				frappe.get_doc(
-					{
-						'doctype': 'Attendance',
-						'employee': emp.name,
-						'fiscal_year':default_fiscal_year,
-						'status':'Absent',
-						'attendance_date':datetime.today().date(),
-						'late' : None,
-						'leave_early' : None,
-						'finger_print': 1,
-						'employee_device_id':emp.attendance_device_id,
-						'department':emp.department,
-						'photo':emp.photo
-					}).save()
-				frappe.db.commit()
+		day_off = frappe.db.sql("""select name from `tabHoliday Schedule` where parent = '{0}' and date = '{1}' and is_day_off = 1""".format(emp.holiday,datetime.now().date()))
+		if len(day_off)==0:
+			frappe.get_doc(
+				{
+					'doctype': 'Attendance',
+					'employee': emp.name,
+					'fiscal_year':default_fiscal_year,
+					'status':'Absent',
+					'attendance_date':datetime.today().date(),
+					'late' : None,
+					'leave_early' : None,
+					'finger_print': 1,
+					"shift":emp.shift,
+					'employee_device_id':emp.attendance_device_id,
+					'department':emp.department,
+					'photo':emp.photo
+				}).save()
+			frappe.db.commit()
 
 @frappe.whitelist()
 def insert_out_attendance():
