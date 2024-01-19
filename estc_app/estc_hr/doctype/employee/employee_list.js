a15317b54408961969347e76228e0ceb978ce3ea
outdomleap <outdom.leap@esoftix.com>
2024-01-19 16:19:14 +0700

    update

--- estc_app/estc_hr/doctype/employee/employee_list.js
+++ estc_app/estc_hr/doctype/employee/employee_list.js
@@ -1,10 +1,21 @@
 frappe.listview_settings['Employee'] = {
     hide_name_column: true,
+    
     formatters: {
         photo: function (value, field, doc) {
             return `<img src='${doc.photo || "/assets/estc_app/images/avatar-2.png"}' style='border-radius: 50%;height:35px; margin-right:10px;margin-left:5px'/>`;
         },
 
+    },
+    get_indicator(doc) {
+        if (doc.status.includes("Active"))
+            return  [`<span style="font-size: 12px;background-color:#17ab07; color:white; padding: 2px 10px;border-radius: 10px;">${__(doc.status)}</span>`]; 
+        if (doc.status.includes("Suspended"))
+            return  [`<span style="font-size: 12px;background-color:#ebca0e; color:white; padding: 2px 10px;border-radius: 10px;">${__(doc.status)}</span>`]; 
+        if (doc.status.includes("Inactive"))
+            return  [`<span style="font-size: 12px;background-color:#de981f; color:white; padding: 2px 10px;border-radius: 10px;">${__(doc.status)}</span>`]; 
+        if (doc.status.includes("Left"))
+            return  [`<span style="font-size: 12px;background-color:#1f1e1c; color:white; padding: 2px 10px;border-radius: 10px;">${__(doc.status)}</span>`]; 
     },
     onload: function (listview) {
         listview.page.add_action_item(__("Assign To Shift"), () => {
